2d7a426eeed3e842d21faa6442868b066036a66d
Brandon Myles <bmyles1006@gmail.com>
2019-04-09 14:59:17 -0500

    Game Setup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # RPS GUI
+
+
+Practice
+
+Manipulating web pages is the primary benefit of the JavaScript language! These techniques are things that you are likely to be messing with every day as a front-end developer, so let’s practice!
+
+    1. Do the first exercise in Wes Bos’s JavaScript30 program by cloning the repo at https://github.com/wesbos/JavaScript30. Check out the Video Tutorial for instructions on the project.
+    2. Go back to your “Rock Paper Scissors” game from a previous lesson and give it a simple UI so that the player can play the game by clicking on buttons rather than typing their answer in a prompt.
+        1. Copy your original code into a new file so you don’t lose it.
+
+        2. For now, remove the logic that plays exactly five rounds.
+
+        3. Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+        
+        4. Add a div for displaying results and change all of your console.logs into DOM methods.
+
+        5. Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+        6. You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of the programmer’s life.
+        
+        7. Don’t forget to go back and add your new version to the original Rock Paper Scissors Project!
---
+++ RPS.js
@@ -0,0 +1,102 @@
+let cpuScore = 0;
+let playScore = 0;
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click'), (e) => {
+        playRound();
+    }
+})
+
+let welcomeMessage = () => {
+    alert `Welcome to my game of Rock Paper Scissors!`;
+    alert `Please enter either: Rock, Paper, or Scissors`;
+}
+
+let computerPlay = () => {
+    let choice = Math.floor((Math.random() * 3) + 1);
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+    return choice;
+};
+
+let playerSelection = () => {
+    let playerchoice = prompt('What is your choice?');
+    return playerchoice.toLowerCase();
+}
+
+let playRound = (computerSelection, selection) => {
+    if (computerSelection === 'paper' && selection === 'rock') {
+        alert("You choose rock, the CPU choose paper, you LOSE!");
+        return 2;
+    } else if (computerSelection === 'rock' && selection === 'scissors') {
+        alert("You choose scissors, the CPU choose rock, you LOSE!");
+        return 2;
+    } else if (computerSelection === 'scissors' && selection === 'paper') {
+        alert("You choose paper, the CPU choose scissors, you LOSE!");
+        return 2;
+    } else if (selection === 'paper' && computerSelection === 'rock') {
+        alert("You choose rock, the CPU choose paper, you WIN!");
+        return 1;
+    } else if (selection === 'rock' && computerSelection === 'scissors') {
+        alert("You choose rock, the CPU choose scissors, you WIN!");
+        return 1;
+    } else if (selection === 'scissors' && computerSelection === 'paper') {
+        alert("You choose scissors, the CPU choose paper, you WIN!");
+        return 1;
+    } else if (selection === computerSelection) {
+        alert("TIE! You both chose the same thing. Play again.");
+        return 0;
+    } else {
+        alert("Invalid response, please enter again.");
+        return 0;
+
+    }
+};
+
+let updateScores = (result, points) => {
+    if (result === 1) {
+        playScore += points;
+        confirm("The score is You: " + playScore + " CPU: " + cpuScore);
+    }
+    if (result === 2) {
+        cpuScore += points;
+        confirm("The score is You: " + playScore + " CPU: " + cpuScore);
+    }
+    if (result === 0) {
+        cpuScore += 0;
+        playScore += 0;
+        confirm("The score is You: " + playScore + " CPU: " + cpuScore);
+    }
+}
+
+let clearGame = () => {
+    let cpuScore = 0;
+    let playScore = 0;
+}
+
+
+
+let game = () => {
+    welcomeMessage();
+
+    let playa = playerSelection();
+    let puter = computerPlay()
+    let result = playRound(puter, playa);
+    updateScores(result, 1);
+
+    if (playScore > cpuScore) {
+        alert("You scored " + playScore + " and the CPU scored " + cpuScore + ", you win boiiii!!!!")
+    } else {
+        alert("The CPU scored " + cpuScore + " and you scored " + playScore + ", you lose!!!!")
+    };
+
+    clearGame();
+}
+
+game();
