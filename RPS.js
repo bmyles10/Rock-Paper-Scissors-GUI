adce49a7f02d2f007c2676bfea9b609799c03ce5
bmyles10 <bmyles1006@gmail.com>
2019-04-19 18:19:39 -0500

    RPS Version 1.0 Complete

--- RPS.js
+++ RPS.js
@@ -1,5 +1,3 @@
-let score = document.createElement('p');
-
 let cpuScore = 0;
 let playScore = 0;
 
@@ -21,52 +19,88 @@ let playRound = (event) => {
     let selection = event.target.id;
 
     if (computerSelection === 'paper' && selection === 'rock') {
-        alert("You choose rock, the CPU choose paper, you LOSE!");
         ++cpuScore;
-        console.log('CPU Won ' + cpuScore)
+        score.textContent = `CPU chose paper, you chose rock, you lose. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (computerSelection === 'rock' && selection === 'scissors') {
-        alert("You choose scissors, the CPU choose rock, you LOSE!");
         ++cpuScore;
-        console.log('CPU Won ' + cpuScore)
+        score.textContent = `CPU chose rock, you chose scissors. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (computerSelection === 'scissors' && selection === 'paper') {
-        alert("You choose paper, the CPU choose scissors, you LOSE!");
         ++cpuScore;
-        console.log('CPU Won ' + cpuScore)
+        score.textContent = `CPU chose scissors, you chose paper. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (selection === 'paper' && computerSelection === 'rock') {
-        alert("You choose rock, the CPU choose paper, you WIN!");
         ++playScore;
-        console.log('Player Won ' + playScore)
+        score.textContent = `You chose paper, CPU chose rock. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (selection === 'rock' && computerSelection === 'scissors') {
-        alert("You choose rock, the CPU choose scissors, you WIN!");
         ++playScore;
-        console.log('Player Won ' + playScore)
+        score.textContent = `You chose rock, CPU chose scissors. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (selection === 'scissors' && computerSelection === 'paper') {
-        alert("You choose scissors, the CPU choose paper, you WIN!");
         ++playScore;
-        console.log('Player Won ' + playScore)
+        score.textContent = `You chose scissors, CPU chose paper. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else if (selection === computerSelection) {
-        alert("TIE! You both chose the same thing. Play again.");
-        return 0;
+        score.textContent = `Tie! Choose again. 
+        CPU - ${cpuScore}
+        You - ${playScore}`;
     } else {
         alert("Invalid response, please enter again.");
-        return 0;
-
     }
+
+    let reloadWindow = () => {
+        location.reload();
+    }
+
 
     let clearGame = () => {
         cpuScore = 0;
         playScore = 0;
+        let clean = document.getElementById('container');
+        clean.remove();
+        let button = document.createElement("button");
+        button.textContent = "Restart Game"; let btn = document.getElementById('btn')
+        btn.appendChild(button); button.addEventListener("click", reloadWindow);
     }
 
-    if (playScore == 3) {
-        alert("You have won the game!")
-        clearGame();
+
+    if (playScore == 2 && cpuScore == 2) {
+        //Not assigning varaible so text doesn't keep popping up.
+        hype.textContent = `Oh Shit! We gotta close one here! NEXT POINT WINS!`
+
+    } else if (playScore == 3) {
+        let para = document.createElement("p");
+        para.innerText = "You have won the game";
+        let outcome = document.getElementById('score')
+        outcome.appendChild(para);
+        let clearHype = document.getElementById('hype');
+        clearHype.remove();
+        clearGame();        
         console.clear();
+
+
     } else if (cpuScore == 3) {
-        alert("You Lost, try again ")
+        let para = document.createElement("p");
+        para.innerText = "Damn Man, You lost";
+        let outcome = document.getElementById('score')
+        outcome.appendChild(para);
+        let clearHype = document.getElementById('hype');
+        clearHype.remove();
         clearGame();
         console.clear();
+
+
+
     }
+
 };
 
 let buttons = document.querySelectorAll('button');
